f035c28ccb88edf3333b111efcdaf17935e0e1f5
passor <passor.catic@gmail.com>
2022-08-24 13:11:39 +0200

    aplha2

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/Logo_Morpion.png">
+  <HelloWorld msg="Un jeu qui gratte un peu et qui comporte des règles:" />
+  <input type="button" value="Entrer" />
 </template>
 
 <script>
+/*let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = () => {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    console.log(JSON.parse(xhr.response));
+  }
+};
+xhr.open('GET', 'https://api.github.com/users/passor53');
+xhr.send(null); */
+
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
