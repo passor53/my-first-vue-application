26a3c851a7084fde3f97e0c94326ef4b1f76f1b8
passor <passor.catic@gmail.com>
2022-08-29 16:32:49 +0200

    Beta1

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,170 @@
 <template>
-  <img alt="Vue logo" src="./assets/Logo_Morpion.png">
-  <HelloWorld msg="Un jeu qui gratte un peu et qui comporte des règles:" />
-  <input type="button" value="Entrer" />
+  <img src="./assets/Logo_Morpion.png" alt="Logo_morpion">
+  <HelloWorld msg="Ça gratte, mais ça se joue aussi ! " />
 </template>
 
 <script>
-/*let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = () => {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    console.log(JSON.parse(xhr.response));
-  }
-};
-xhr.open('GET', 'https://api.github.com/users/passor53');
-xhr.send(null); */
-
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
   }
 }
+
 </script>
 
 <style>
+body {
+  text-align: center;
+  background-image: url('./assets/Fond-ecran.jpg');
+  background-repeat: no-repeat;
+  background-size: 100vw 100vh;
+}
+
+.rdj {
+  position: relative;
+  border: solid 1px gray;
+  border-radius: 1vw;
+  margin: 0 45vw 0 45vw;
+  color: orange;
+  background-color: rgb(48, 47, 47);
+}
+
+.regles {
+  position: absolute;
+  transition: opacity .5s;
+  opacity: 0;
+  top: 25vh;
+  left: 15vw;
+  border: solid 2px whitesmoke;
+  border-style: outset;
+  border-radius: 3vw;
+  background-color: rgb(48, 47, 47);
+  color: orange;
+  text-align: center;
+  width: 70vw;
+}
+
+.regles:hover {
+  opacity: 90;
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #030302;
   margin-top: 60px;
+}
+
+.case {
+  border: 3px outset gray;
+  background-color: rgb(245, 245, 245);
+
+  height: 100px;
+  line-height: 100px;
+  font-size: 60px;
+}
+
+#jeu {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 300px;
+  margin: auto;
+}
+
+.hello {
+  color: rgb(0, 0, 32);
+}
+
+.players {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+  margin: 0 18vw 0 18vw;
+}
+
+.titre2 {
+  border: solid 1px gray;
+  background-color: rgb(48, 47, 47);
+  border-radius: 5vw;
+  margin: 0 5vw 0 5vw;
+  margin-top: 10vh;
+  color: orange;
+}
+
+.label {
+  color: orange;
+  border: solid 1px gray;
+  background-color: rgb(48, 47, 47);
+  border-radius: 2vw;
+  padding: 0 1vw 0 1vw;
+}
+
+.joueur1 {
+  background-image: url(./assets/blaster.png);
+  background-size: 27vw;
+  background-repeat: no-repeat;
+  width: 50vw;
+  height: auto;
+  border-radius: 5vh;
+  height: 40vh;
+  background-position: center;
+  margin-right: 5vw;
+  margin-left: -5vw;
+}
+
+
+.joueur2 {
+  background-image: url(./assets/blaster.png);
+  background-size: 27vw;
+  background-repeat: no-repeat;
+  width: 50vw;
+  height: auto;
+  border-radius: 5vh;
+  height: 40vh;
+  background-position: center;
+  margin-left: 5vw;
+  margin-right: -5vw;
+}
+
+.envoyer {
+  margin: 16.5vh 0 16.5vh 0;
+  border-radius: 5vw;
+  border-color: gray;
+  border-style: solid;
+  background-color: rgb(48, 47, 47);
+  color: orange;
+}
+
+.affichage_joueur {
+  text-transform: uppercase;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: orange;
+  border: solid 1px gray;
+  background-color: rgb(48, 47, 47);
+}
+
+.rejouer {
+  justify-self: center;
+  align-self: center;
+  border: solid 1px gray;
+  border-radius: 4vw;
+  background-image: url(./assets/refresh.png);
+  background-color: rgb(48, 47, 47);
+  background-size: 5vw 5vh;
+  background-repeat: no-repeat;
+  margin-top: 2vh;
+  color: orange;
+  padding: 0 5vw 3vh 0;
+  cursor: pointer;
+  text-decoration: solid;
+
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,53 +1,118 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="bloc_page">
+    <p class="rdj" v-if="table == undefined">Règles</p>
+    <p class="regles" v-if="table == undefined">
+      <br>RÈGLE DU JEU <br><br>
+      - Jeu qui se joue à deux joueurs, sur un damier de 3 cases par 3 cases.<br />
+      - Chaque joueur est représenté par un "symbole".<br />
+      - Un joueur utilise toujours le même type de "symbole".<br />
+      - Un premier joueur dessine son symbole sur une case,<br /> puis c'est au tour de l'autre joueur de dessiner
+      son
+      symbole
+      sur une case vide.<br />
+      - Le but du jeu est de réussir à aligner ses trois symboles (horizontal, vertical ou diagonale), on remporte
+      alors
+      la partie.<br><br>
+    </p>
+    <div class="hello">
+      <h1>{{  msg  }}</h1>
+    </div>
+
+    <div id="jeu" v-if="table != undefined">
+      <div v-for="i in 9" :key="i" :data-value=i v-on:click="cycle_jeu($event)" class="case"></div>
+
+      <p class="affichage_joueur">{{  this.pseudo1  }} : {{  this.jeton1  }}</p>
+      <input class="rejouer" type="button" title="Rejouer" onclick='window.location.reload(false)' />
+      <p class="affichage_joueur">{{  this.pseudo2  }} : {{  this.jeton2  }}</p>
+    </div>
+
+    <div class="players" v-if="table == undefined">
+
+      <div class="joueur1">
+        <h2 class="titre2">Joueur 1</h2>
+        <h4>
+          <label class="label" for="pseudo1">Pseudo :</label>
+          <input type="text" name="pseudo" id="pseudo1" maxlength="8" />
+        </h4>
+        <select name="jeton1" id="jeton1">
+          <option value="X">X</option>
+          <option value="O">O</option>
+        </select>
+      </div>
+      <input class="envoyer" type="button" value="Jouer" v-on:click="send" />
+
+      <div class="joueur2">
+        <h2 class="titre2">Joueur 2</h2>
+        <h4>
+          <label class="label" for="pseudo2">Pseudo :</label>
+          <input type="text" name="pseudo" id="pseudo2" maxlength="8" />
+        </h4>
+        <select name="jeton2" id="jeton2">
+          <option value="X">X</option>
+          <option value="O">O</option>
+        </select>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-/*import fetch from 'fetch';
 
-fetch('https://github.com/passor53')
-  .then((data) => data.json())
-  .then((user) => {
-    console.log(user);
-  });
-  */
-/*axios.get('http://localhost:8080/')
-  .then((reponse) => {
-    console.log(reponse.data);
-  });
-*/
-/*axios.post('https://www.site.com', { age: 30 })
-  .then((reponse) => {
-    console.log(reponse.data);
-  });
-*/
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+
+  data() {
+    return {
+      table: undefined,
+      pseudo1: '',
+      pseudo2: '',
+      jeton1: '',
+      jeton2: '',
+      tourjeu: 1,
+    }
+
+  },
+
+
+  methods: {
+    send() {
+      this.pseudo1 = document.getElementById('pseudo1').value;
+      this.table = true;
+      console.log(this.pseudo1)
+
+      this.jeton1 = document.getElementById('jeton1').value;
+      console.log(this.jeton1)
+
+      this.pseudo2 = document.getElementById('pseudo2').value;
+      this.table = true;
+      console.log(this.pseudo2)
+
+      this.jeton2 = document.getElementById('jeton2').value;
+      console.log(this.jeton2)
+    },
+
+
+    cycle_jeu(event) {
+
+      if (this.tourjeu % 2 == 1) { event.target.innerHTML = this.jeton1; }
+      if (this.tourjeu % 2 == 0) { event.target.innerHTML = this.jeton2; }
+
+      let val = event.target.getAttribute('data-value');
+      console.log(val);
+
+      this.tourjeu = this.tourjeu + 1;
+      this.table.find(val == val)
+      val = this.table = Array.splice(val, 1, 'X')
+    }
   }
+
 }
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
